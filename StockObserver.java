92a160374b3d09c017bd9eabc2bcf0074c913713
cspatterns <dragontbone@gmail.com>
2016-05-07 12:15:07 +0100

    Add observerIDTracker and observerID
    Add observerIDTracker and observerID. These variables will be use to
    track Observers.

--- StockObserver.java
+++ StockObserver.java
@@ -4,12 +4,24 @@ public class Stockobserver implements Observer{
 	private double googPrice;
 	private double aaplPrice;
 
+    // Static used as a counter
+	private static int observerIDTracker = 0; 
+
+
+    // use to track Observers
+	private int observerID;
+
 	private Subject stockGrabber;
 
 	private int ObserverTracker = 0;
 
 	public StockObserver(Subject stockGrabber){
 		this.stockGrabber = stockGrabber;
+
+		this.observerID = ++observerIDTracker;
+
+		System.out.println("New Observer + this.observerID");
+
 		stockGrabber.register(this);
 	}
 
