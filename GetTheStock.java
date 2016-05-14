f1ff3163d871eadaa0b3f7e926c24f26140a0b81
Jay D. Foer <dragontbone@gmail.com>
2016-05-14 10:11:22 +0100

    GetTheStock - second attempt

--- GetTheStock.java
+++ GetTheStock.java
@@ -20,11 +20,29 @@ public class GetTheStock impelements Runnable{
 		for (int i - 0; i < 20; i++){
 			
 			// get new Decimal Format
+			DecimalFormat df = new DecimalFormat("#.###");
 			
 			
 			//generate random numbers between 0.6 and -0.3
+			double random = Math.random() * 0.6 - 0.3;
 			
-			//set the price
+			
+			
+			//set the price for each stock using if statement e.g.
+			//if price is IBM
+			
+			if(symbol == "IBM"){
+				stockgrabber.setIBMPrice(df(price + random));
+			}
+			
+			if(symbol == "GOOG"){
+				stockgrabber.setGOOGPrice(df(price + random));
+			}
+			
+			
+			if(symbol == "AAPL"){
+				stockgrabber.setAAPLPrice(df(price + random));
+			}
 			
 		}
 	}
